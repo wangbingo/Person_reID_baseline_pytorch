4df3bae6e3920f5810e7da128a7ba60daf7c5d78
王兵 <wangbing@102.1.68.92.in-addr.arpa>
2020-09-22 02:17:03 +0800

    split train val btw dirs

--- my_prepare_ending.py
+++ my_prepare_ending.py
@@ -11,7 +11,7 @@ download_path = '../train'
 def moveFile(srcDir, dstDir, rate = 0.1):
     pathDir = os.listdir(srcDir)    #取图片的原始路径
     filenumber=len(pathDir)
-    if filenumber <= 3:  return
+    if filenumber <= 3:  return   # train : val = 4 : 1
     #rate=0.1    #自定义抽取图片的比例，比方说100张抽10张，那就是0.1
     picknumber=max(1, int(filenumber * rate)) #按照rate比例从文件夹中取一定数量图片
     sample = random.sample(pathDir, picknumber)  #随机选取picknumber数量的样本图片
@@ -32,12 +32,15 @@ os.system('cp -r ../train/pytorch/train_all/* ../train/pytorch/train/')  # teste
 print('cp train_all train completed........')
 
 split_rate = 0.1
-c = 0
-for root, dirs, files in os.walk(train_save_path, topdown=True):
-    for dir in dirs:
-        # os.mkdir(val_save_path + '/' + dir)
-        moveFile(train_save_path + '/' + dir, val_save_path + '/' + dir, split_rate)
-        c += 1
-        if c % 2000 == 0:
-            print('{} dirs processed.'.format(c))
+#for root, dirs, files in os.walk(train_save_path, topdown=True):
+#    for dir in dirs:
+
+dir_numbers = len(os.listdir(train_save_path))    # 19658
+pick_numbers = int(dir_numbers * split_rate)      #按照rate比例从dir中取一定数量dir
+dir_samples = random.sample(os.listdir(train_save_path), pick_numbers)  #随机选取picknumber数量的dir
+
+for dir in dir_samples:
+    shutil.move(train_save_path + '/' + dir, val_save_path + '/' + dir)
+    
+print('{} / {} dirs moved.'.format(len(dir_samples), dir_numbers)
 print('train/val  datasets completed.  split rate is {}'.format(split_rate))

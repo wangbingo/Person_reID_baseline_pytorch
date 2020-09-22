051b7c0cbab30fbeaabd42afe1dcf7985b7d20c5
王兵 <wangbing@92-68-1-105.biz.kpn.net>
2020-09-22 11:08:02 +0800

    my_prepare_ending.py -> my_prepare.py

--- my_prepare.py
+++ my_prepare.py
@@ -1,4 +1,4 @@
-import os
+import os, random, shutil
 from shutil import copyfile
 from IPython import embed
 
@@ -102,27 +102,28 @@ for line in lines:
 print('train_all dataset completed.  {} files copied.'.format(c))
 
 
-
 #---------------------------------------
 #train_val
+
 train_save_path = download_path + '/pytorch/train'
 val_save_path = download_path + '/pytorch/val'
+
 if not os.path.isdir(train_save_path):
     os.mkdir(train_save_path)
     os.mkdir(val_save_path)
 
+print('cp train_all train begin........')
 os.system('cp -r ../train/pytorch/train_all/* ../train/pytorch/train/')  # tested ok.
+print('cp train_all train completed........')
 
-for root, dirs, files in os.walk(train_save_path, topdown=True):
-    for dir in dirs:
-        
-        if not name[-3:]=='jpg':
-            continue
-        ID  = name.split('_')
-        src_path = train_path + '/' + name
-        dst_path = train_save_path + '/' + ID[0]
-        if not os.path.isdir(dst_path):
-            os.mkdir(dst_path)
-            dst_path = val_save_path + '/' + ID[0]  #first image is used as val image
-            os.mkdir(dst_path)
-        copyfile(src_path, dst_path + '/' + name)
+split_rate = 0.1
+
+dir_numbers = len(os.listdir(train_save_path))    # 19658
+pick_numbers = int(dir_numbers * split_rate)      #
+dir_samples = random.sample(os.listdir(train_save_path), pick_numbers)  #
+
+for dir in dir_samples:
+    shutil.move(train_save_path + '/' + dir, val_save_path + '/' + dir)
+
+print('{} / {} dirs moved from train to val.'.format(len(dir_samples), dir_numbers))
+print('train/val datasets generated.')

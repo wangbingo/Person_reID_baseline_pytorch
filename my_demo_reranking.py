39f8fb355a669f5e72764a3ef924477a2e62114e
王兵 <wangbing@92-68-1-107.biz.kpn.net>
2020-09-24 18:50:20 +0800

    my_demo & my_demo_reranking

---
+++ my_demo_reranking.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python2/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 26 14:46:56 2017
+@author: luohao
+Modified by Houjing Huang, 2017-12-22. 
+- This version accepts distance matrix instead of raw features. 
+- The difference of `/` division between python 2 and 3 is handled.
+- numpy.float16 is replaced by numpy.float32 for numerical precision.
+
+Modified by Zhedong Zheng, 2018-1-12.
+- replace sort with topK, which save about 30s.
+"""
+
+"""
+CVPR2017 paper:Zhong Z, Zheng L, Cao D, et al. Re-ranking Person Re-identification with k-reciprocal Encoding[J]. 2017.
+url:http://openaccess.thecvf.com/content_cvpr_2017/papers/Zhong_Re-Ranking_Person_Re-Identification_CVPR_2017_paper.pdf
+Matlab version: https://github.com/zhunzhong07/person-re-ranking
+"""
+
+"""
+API
+q_g_dist: query-gallery distance matrix, numpy array, shape [num_query, num_gallery]
+q_q_dist: query-query distance matrix, numpy array, shape [num_query, num_query]
+g_g_dist: gallery-gallery distance matrix, numpy array, shape [num_gallery, num_gallery]
+k1, k2, lambda_value: parameters, the original paper is (k1=20, k2=6, lambda_value=0.3)
+Returns:
+  final_dist: re-ranked distance, numpy array, shape [num_query, num_gallery]
+"""
+
+
+import numpy as np
+
+def k_reciprocal_neigh( initial_rank, i, k1):
+    forward_k_neigh_index = initial_rank[i,:k1+1]
+    backward_k_neigh_index = initial_rank[forward_k_neigh_index,:k1+1]
+    fi = np.where(backward_k_neigh_index==i)[0]
+    return forward_k_neigh_index[fi]
+
+def re_ranking(q_g_dist, q_q_dist, g_g_dist, k1=20, k2=6, lambda_value=0.3):
+    # The following naming, e.g. gallery_num, is different from outer scope.
+    # Don't care about it.
+    original_dist = np.concatenate(
+      [np.concatenate([q_q_dist, q_g_dist], axis=1),
+       np.concatenate([q_g_dist.T, g_g_dist], axis=1)],
+      axis=0)
+    original_dist = 2. - 2 * original_dist   # change the cosine similarity metric to euclidean similarity metric
+    original_dist = np.power(original_dist, 2).astype(np.float32)
+    original_dist = np.transpose(1. * original_dist/np.max(original_dist,axis = 0))
+    V = np.zeros_like(original_dist).astype(np.float32)
+    #initial_rank = np.argsort(original_dist).astype(np.int32)
+    # top K1+1
+    initial_rank = np.argpartition( original_dist, range(1,k1+1) )
+
+    query_num = q_g_dist.shape[0]
+    all_num = original_dist.shape[0]
+
+    for i in range(all_num):
+        # k-reciprocal neighbors
+        k_reciprocal_index = k_reciprocal_neigh( initial_rank, i, k1)
+        k_reciprocal_expansion_index = k_reciprocal_index
+        for j in range(len(k_reciprocal_index)):
+            candidate = k_reciprocal_index[j]
+            candidate_k_reciprocal_index = k_reciprocal_neigh( initial_rank, candidate, int(np.around(k1/2)))
+            if len(np.intersect1d(candidate_k_reciprocal_index,k_reciprocal_index))> 2./3*len(candidate_k_reciprocal_index):
+                k_reciprocal_expansion_index = np.append(k_reciprocal_expansion_index,candidate_k_reciprocal_index)
+
+        k_reciprocal_expansion_index = np.unique(k_reciprocal_expansion_index)
+        weight = np.exp(-original_dist[i,k_reciprocal_expansion_index])
+        V[i,k_reciprocal_expansion_index] = 1.*weight/np.sum(weight)
+
+    original_dist = original_dist[:query_num,]
+    if k2 != 1:
+        V_qe = np.zeros_like(V,dtype=np.float32)
+        for i in range(all_num):
+            V_qe[i,:] = np.mean(V[initial_rank[i,:k2],:],axis=0)
+        V = V_qe
+        del V_qe
+    del initial_rank
+    invIndex = []
+    for i in range(all_num):
+        invIndex.append(np.where(V[:,i] != 0)[0])
+
+    jaccard_dist = np.zeros_like(original_dist,dtype = np.float32)
+
+    for i in range(query_num):
+        temp_min = np.zeros(shape=[1,all_num],dtype=np.float32)
+        indNonZero = np.where(V[i,:] != 0)[0]
+        indImages = []
+        indImages = [invIndex[ind] for ind in indNonZero]
+        for j in range(len(indNonZero)):
+            temp_min[0,indImages[j]] = temp_min[0,indImages[j]]+ np.minimum(V[i,indNonZero[j]],V[indImages[j],indNonZero[j]])
+        jaccard_dist[i] = 1-temp_min/(2.-temp_min)
+
+    final_dist = jaccard_dist*(1-lambda_value) + original_dist*lambda_value
+    del original_dist
+    del V
+    del jaccard_dist
+    final_dist = final_dist[:query_num,query_num:]
+    return final_dist
+
+
+
+
+import argparse
+import scipy.io
+import torch
+# import numpy as np
+import os
+from torchvision import datasets
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import json
+#######################################################################
+# Evaluate
+parser = argparse.ArgumentParser(description='Demo')
+parser.add_argument('--query_index', default=777, type=int, help='test_image_index')
+parser.add_argument('--test_dir',default='../train/pytorch',type=str, help='./test_data')
+opts = parser.parse_args()
+
+data_dir = opts.test_dir
+image_datasets = {x: datasets.ImageFolder( os.path.join(data_dir,x) ) for x in ['gallery','query']}
+
+#####################################################################
+#Show result
+def imshow(path, title=None):
+    """Imshow for Tensor."""
+    im = plt.imread(path)
+    plt.imshow(im)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+######################################################################
+result = scipy.io.loadmat('pytorch_result.mat')
+query_feature = torch.FloatTensor(result['query_f'])
+query_cam = result['query_cam'][0]
+query_label = result['query_label'][0]
+gallery_feature = torch.FloatTensor(result['gallery_f'])
+gallery_cam = result['gallery_cam'][0]
+gallery_label = result['gallery_label'][0]
+
+multi = os.path.isfile('multi_query.mat')
+
+if multi:
+    m_result = scipy.io.loadmat('multi_query.mat')
+    mquery_feature = torch.FloatTensor(m_result['mquery_f'])
+    mquery_cam = m_result['mquery_cam'][0]
+    mquery_label = m_result['mquery_label'][0]
+    mquery_feature = mquery_feature.cuda()
+
+query_feature = query_feature.cuda()
+gallery_feature = gallery_feature.cuda()
+
+#######################################################################
+# sort the images
+def sort_img(qf, ql, qc, gf, gl, gc):
+    query = qf.view(-1,1)
+    # print(query.shape)
+    score = torch.mm(gf,query)
+    score = score.squeeze(1).cpu()
+    score = score.numpy()
+    # predict index
+    index = np.argsort(score)  #from small to large
+    index = index[::-1]
+    # index = index[0:2000]
+    return index
+
+result_dict = {}
+
+q_index = opts.query_index  # query total number: 2900
+
+for i in range(q_index):
+
+    index = sort_img(query_feature[i],query_label[i],query_cam[i],gallery_feature,gallery_label,gallery_cam)
+
+    query_path, _ = image_datasets['query'].imgs[i]
+    #     query_path = '../train/pytorch/query/11/00002570.png'
+    query_path = query_path.split('/')[-1] # get '00002570.png'
+
+    img_path_list = []
+    for j in range(200):                    # top-200
+        img_path, _ = image_datasets['gallery'].imgs[index[j]]
+        #       img_path = '../train/pytorch/gallery/99/00108716.png'
+        img_path = img_path.split('/')[-1]       # get '00108716.png'
+        img_path_list.append(img_path)
+
+    result_dict[query_path] = img_path_list
+    if i % 100 == 0:
+        print('{}/{} processed..........'.format(i+1, q_index))
+
+import datetime
+nowTime = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+
+with open('result_' + str(nowTime) + '.json','w') as fp:
+    json.dump(result_dict, fp, indent = 4, separators=(',', ': '))
+    # json.dump(result_dict, fp)
+
+print('The result generated................')

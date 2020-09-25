3b26dafa0b849f9491a26be431027273b8318ebf
王兵 <wangbing@105.1.68.92.in-addr.arpa>
2020-09-26 00:18:31 +0800

    gf=15000~30000

--- my_demo_reranking.py
+++ my_demo_reranking.py
@@ -151,7 +151,7 @@ if multi:
 
 query_feature = query_feature.cuda()
 
-gallery_feature = gallery_feature[:15000,:]    # only  rerank  0-2999
+gallery_feature = gallery_feature[15000:30000,:]    # only  rerank  0-2999
 gallery_feature = gallery_feature.cuda() 
 
 #######################################################################
@@ -197,7 +197,7 @@ final_q_g_dist = re_ranking(q_g_distance, q_q_distance, g_g_distance, k1=20, k2=
 
 # Save to file
 final_q_g_dist_dict = {'final_q_g' : final_q_g_dist}
-scipy.io.savemat('final_q_g_dist.mat', final_q_g_dist_dict)
+scipy.io.savemat('final_q_g_dist_2.mat', final_q_g_dist_dict)
 
 print('finish.............')
 embed()

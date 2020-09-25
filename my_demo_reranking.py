b8cf2be3829f1336bbb1f4c67035ca02eed1c649
王兵 <wangbing@105.1.68.92.in-addr.arpa>
2020-09-26 00:10:18 +0800

    gf=15000

--- my_demo_reranking.py
+++ my_demo_reranking.py
@@ -151,7 +151,7 @@ if multi:
 
 query_feature = query_feature.cuda()
 
-gallery_feature = gallery_feature[:13489,:]    # only  rerank  0-2999
+gallery_feature = gallery_feature[:15000,:]    # only  rerank  0-2999
 gallery_feature = gallery_feature.cuda() 
 
 #######################################################################
